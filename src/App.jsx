35c63c31b10ce25a9951bb38a58968641b000f0f
Adarsh <ad0143maurya@gmail.com>
2024-02-17 12:23:37 +0530

    Co-authored-by: Sidrawat2003 <Sidrawat2003@users.noreply.github.com> Co-authored-by: Shekhar Kumar <Shekhar389@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router ,Routes ,Route } from "react-router-dom";
+import About from "./components/About";
+import Login from "./components/Login";
 const App = () => {
-    return <>
-        <h1 className="bg-blue-500 text-5xl flex items-center justify-center w-[500px] h-[100px]">adarsh</h1>
-    </>;
+    return (
+        <>
+            
+           
+           
+            <Router> 
+                <Navbar/>
+                <Routes>
+                   
+                    <Route path={"/about"} element={<About/>} />
+                    <Route path={"/login"} element={<Login/>} />
+                    
+
+                </Routes>
+            </Router>
+
+                
+            
+        </>
+    );
 };
 
 export default App;
